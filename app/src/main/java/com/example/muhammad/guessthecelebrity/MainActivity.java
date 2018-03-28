9973e4b2ee5f5ed13867d3ce267f3d96c6c719b4
Sherif negm <sherif.negmeldeen1@gmail.com>
2018-03-29 01:27:14 +0200

    adding choices in button format..a correct answer and 3 random wrong answer

--- app/src/main/java/com/example/muhammad/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/example/muhammad/guessthecelebrity/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.muhammad.guessthecelebrity;
 
+
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,55 +26,110 @@ import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity {
-    ArrayList<String> celebUrls=new ArrayList<String>();
-    ArrayList<String> celebNames=new ArrayList<String>();
-    int Chosenceleb =0;
+
+public class MainActivity extends Activity {
+
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int chosenCeleb = 0;
+    int locationOfCorrectAnswer = 0;
+    String[] answers = new String[4];
+
     ImageView imageView;
-    public class ImageDownloader extends AsyncTask<String,Void,Bitmap>{
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    public void celebChosen(View view) {
+
+        if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+
+            Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            Toast.makeText(getApplicationContext(), "Wrong! It was " + celebNames.get(chosenCeleb), Toast.LENGTH_LONG).show();
+
+        }
+
+        createNewQuestion();
+
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
 
         @Override
         protected Bitmap doInBackground(String... urls) {
+
             try {
-                URL url= new URL(urls[0]);
-                HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
                 connection.connect();
-                InputStream inputStream= connection.getInputStream();
-                Bitmap mybitMap = BitmapFactory.decodeStream(inputStream);
-                return  mybitMap;
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
 
             } catch (MalformedURLException e) {
+
                 e.printStackTrace();
+
             } catch (IOException e) {
+
                 e.printStackTrace();
+
             }
+
             return null;
         }
     }
 
-    public  class DownloadTask extends AsyncTask<String,Void,String>{
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
 
         @Override
         protected String doInBackground(String... urls) {
-            String result="";
-            URL url;
-            HttpURLConnection connection=null;
-            try {
-                url =new  URL(urls[0]);
-                connection=(HttpURLConnection) url.openConnection();
-                InputStream in =connection.getInputStream();
-                InputStreamReader reader =new InputStreamReader(in);
-                int data =reader.read();
-                while (data!= -1){
-                    char current =(char)data;
-                    result+=current;
-                    data=reader.read();
-                }
-                return  result;
 
-            }
-            catch (Exception e){
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+
                 e.printStackTrace();
+
             }
 
             return null;
@@ -79,11 +140,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView =(ImageView) findViewById(R.id.imageView);
-        DownloadTask task= new DownloadTask();
-        String result= null;
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
 
         try {
+
             result = task.execute("http://www.posh24.se/kandisar").get();
 
             String[] splitResult = result.split("<div class=\"sidebarContainer\">");
@@ -93,26 +161,88 @@ public class MainActivity extends AppCompatActivity {
 
             while (m.find()) {
 
-                celebUrls.add(m.group(1));
+                celebURLs.add(m.group(1));
 
             }
-            p= Pattern.compile("alt=\"(.*?)\"");
-            m=p.matcher(splitResult[1]);
 
-            while (m.find()){
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+
                 celebNames.add(m.group(1));
+
             }
-            Random random= new Random();
-            Chosenceleb = random.nextInt(celebUrls.size());
-            ImageDownloader imageTask= new ImageDownloader();
-            Bitmap celebImage;
-            celebImage = imageTask.execute(celebUrls.get(Chosenceleb)).get();
-            imageView.setImageBitmap(celebImage);
+
 
         } catch (InterruptedException e) {
+
             e.printStackTrace();
+
         } catch (ExecutionException e) {
+
+            e.printStackTrace();
+
+        }
+
+        createNewQuestion();
+
+    }
+
+    public void createNewQuestion() {
+
+        Random random = new Random();
+        chosenCeleb = random.nextInt(celebURLs.size());
+
+        ImageDownloader imageTask = new ImageDownloader();
+
+        Bitmap celebImage;
+
+        try {
+
+            celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+
+            imageView.setImageBitmap(celebImage);
+
+            locationOfCorrectAnswer = random.nextInt(4);
+
+            int incorrectAnswerLocation;
+
+            for (int i = 0; i < 4; i++) {
+
+                if (i == locationOfCorrectAnswer) {
+
+                    answers[i] = celebNames.get(chosenCeleb);
+
+                } else {
+
+                    incorrectAnswerLocation = random.nextInt(celebURLs.size());
+
+                    while (incorrectAnswerLocation == chosenCeleb) {
+
+                        incorrectAnswerLocation = random.nextInt(celebURLs.size());
+
+                    }
+
+                    answers[i] = celebNames.get(incorrectAnswerLocation);
+
+
+                }
+
+
+            }
+
+            button0.setText(answers[0]);
+            button1.setText(answers[1]);
+            button2.setText(answers[2]);
+            button3.setText(answers[3]);
+
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+
     }
+
 }
